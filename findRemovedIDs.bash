6c090713aad0562bb55b7dab15577c9e9fab5a24
Mohammad Parvin <m.parvin986@gmail.com>
2017-10-28 12:33:42 +0330

    Add report file variable, add more comment, set commentID start from 0

--- findRemovedIDs.bash
+++ findRemovedIDs.bash
@@ -1,21 +1,32 @@
 #!/bin/bash
-
 DBUSER=""      # Database User Name
 DBPASS=""      # Database Password
 DBNAME=""      # Database Name
 DBHOST=""      # Database Server IP
 TABLENAME=""      # Table Name
 FieldName=""      # Auto increased field name
+ReportFile=""
+
+>$ReportFile  #Clear old Report
+### get maximum number from database
 lastID=`mysql -u"$DBUSER" -p"$DBPASS" $DBNAME -h$DBHOST -N -e "SELECT MAX($FieldName) FROM $TABLENAME"`
-columnId=1
+### set columnId 0, this variable is use for loop
+columnId=0
+
+### Loop in 0 to maximum id from database
 while [[ $columnId -le $lastID ]]
 do
+### Increasing columnId, because id in mysql start from 1, and this variable used for loop condition
+((columnId = columnId+1))
+### get field id from database where this id is equal to loop number
 dbID=`mysql -u"$DBUSER" -p"$DBPASS" $DBNAME -h$DBHOST -N -e "SELECT $FieldName FROM $TABLENAME WHERE $FieldName=$columnId"`
+### check if loop number is equal to database id, that is exists; then continue to loop
 if [[ "$dbID" == "$columnId" ]]; then
 continue
+### else this loop id is not exists in database so show this id and store it in report file
 else
 echo -e "$columnId not found\n"
-echo -e "$columnId\n" >> notFoundIds.list
+### Append id to report File
+echo -e "$columnId" >> $ReportFile
 fi
-((columnId = columnId+1))
 done
