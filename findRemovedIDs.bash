6209508ac1768881017002fcffd9d58e1c0a112a
Mohammad Parvin <m.parvin986@gmail.com>
2017-10-28 12:03:57 +0330

    Using -N to skip column name

--- findRemovedIDs.bash
+++ findRemovedIDs.bash
@@ -6,11 +6,11 @@ DBNAME=""      # Database Name
 DBHOST=""      # Database Server IP
 TABLENAME=""      # Table Name
 FieldName=""      # Auto increased field name
-lastID=`mysql -u"$DBUSER" -p"$DBPASS" $DBNAME -h$DBHOST -e "SELECT MAX($FieldName) FROM $TABLENAME"`
+lastID=`mysql -u"$DBUSER" -p"$DBPASS" $DBNAME -h$DBHOST -N -e "SELECT MAX($FieldName) FROM $TABLENAME"`
 columnId=1
 while [[ $columnId -le $lastID ]]
 do
-dbID=`mysql -u"$DBUSER" -p"$DBPASS" $DBNAME -h$DBHOST -e "SELECT $FieldName FROM $TABLENAME WHERE $FieldName=$columnId"`
+dbID=`mysql -u"$DBUSER" -p"$DBPASS" $DBNAME -h$DBHOST -N -e "SELECT $FieldName FROM $TABLENAME WHERE $FieldName=$columnId"`
 if [[ "$dbID" == "$columnId" ]]; then
 continue
 else
